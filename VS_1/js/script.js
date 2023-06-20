218aa028d628ac2be10080dda707811af8ee3ca0
hcsaba1983 <118347983+hcsaba1983@users.noreply.github.com>
2023-06-20 15:27:31 +0200

    1523

--- VS_1/js/script.js
+++ VS_1/js/script.js
@@ -19,3 +19,19 @@ function onKivonas() {
     let mennyit = document.getElementById("mennyit").value;
     alert(mibol - mennyit);    
 }
+
+function onGeneralas() {
+
+    let buborek = '<div class="szambuborek">#SZAM#</div> ';
+    let tartalom = '';
+
+    for (let i = 0; i < 20; i++) {
+        let szam = Math.trunc(Math.random() * 100);
+        
+        tartalom += buborek.replace("#SZAM#", szam);
+    }
+
+    document.getElementById("szamok").innerHTML = tartalom;
+
+
+}
