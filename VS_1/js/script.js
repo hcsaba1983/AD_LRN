2f37992963bc3f304824c459d514b564929bb2e4
hcsaba1983 <118347983+hcsaba1983@users.noreply.github.com>
2023-06-20 16:07:27 +0200

    1600

--- VS_1/js/script.js
+++ VS_1/js/script.js
@@ -25,13 +25,43 @@ function onGeneralas() {
     let buborek = '<div class="szambuborek">#SZAM#</div> ';
     let tartalom = '';
 
+    let lista = [];
+
     for (let i = 0; i < 20; i++) {
-        let szam = Math.trunc(Math.random() * 100);
-        
-        tartalom += buborek.replace("#SZAM#", szam);
+                
+        setTimeout(function hozzaad() {
+            let szam = Math.trunc(Math.random() * 100);
+            tartalom = document.getElementById("szamok").innerHTML + buborek.replace("#SZAM#", szam);
+            document.getElementById("szamok").innerHTML = tartalom;
+        }, i * 1000);
+
     }
 
-    document.getElementById("szamok").innerHTML = tartalom;
+    
+
+   
+
+        
+
+    
 
 
+}
+
+function onLetoltTeendok() {
+    let teendo = '<div class="kartya-wrapper col-12 col-md-6 col-lg-4">' + 
+        '<div class="teendo-kartya">' + 
+        '    <div class="teendo-id">#ID#</div>' + 
+        '        <p class="teendo-nev">#NEV#</p>' + 
+        '        <p class="teendo-keszultsege teendo-kesz">KESZ</p>' + 
+        '        <p class="teendo-keszultsege teendo-nyitott">NYITOTT</p>' + 
+        '    </div>' + 
+        '</div>';
+
+
+
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => alert(json))
+    
 }
